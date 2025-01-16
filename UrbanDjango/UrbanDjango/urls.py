9e28847c2e27aef4c1338f30db99518abde7f413
python <kibersantus@mail.ru>
2025-01-16 20:33:50 +0300

    Django2

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from task2.views import func_views, ClassViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('func_template/', func_views),
+    path('class_template/', ClassViews.as_view())
 ]
